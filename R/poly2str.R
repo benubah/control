3685fa4742944e8abf18461f2c676ecdf2c3ba80
benubah <ben@rpowerlabs.org>
2017-05-30 14:04:25 +0100

    added tf and polystr

---
+++ R/poly2str.R
@@ -0,0 +1,65 @@
+## Modified from package *pracma*.
+## Modification: To hide any coefficient and power that is equal to 1
+## So that instead of '1s^3' we have 's^3' and instead of 's^1', we have 's'
+##  p o l y 2 s t r . R  Print Polynomial
+#
+#' @export
+
+
+poly2str <- function(p, svar = "x", smul = "*",
+                     d = options("digits")$digits) {
+  if (length(p) == 0) return("")
+  if (!is.numeric(p))
+    stop("Argument 'p' must be a numeric vector.")
+
+  while (p[1] == 0 && length(p) > 1)
+    p <- p[2:length(p)]
+  if (length(p) == 1) return(as.character(p))
+
+  s <- sign(p)
+  p <- abs(p)
+
+  p <- formatC(p, digits = d)
+  p <- sub("^\\s+", "", p)
+
+  n <- length(p) - 1
+  S <- ""
+  #print(p)
+  s1 <- if (s[1] == 1) "" else "-"
+
+  #added to clean out any first coefficient that is 1. So that instead of 1s^1 we have s^1
+  if(p[1] == "1" && n == 1){
+    S <- paste(s1, "", smul, svar, "", sep = "")
+  } else if(p[1] == "1"){
+    S <- paste(s1, "", smul, svar, "^", n, sep = "")
+  } else {
+    S <- paste(s1, p[1], smul, svar, "^", n, sep = "")
+  }
+
+
+
+  for (i in 2:(n+1)) {
+    if (s[i] == 1) s1 <- " + "
+    else if (s[i] == -1) s1 <- " - "
+    else next
+
+    if (n-i+1 > 1) {
+      #added to clean out any coefficients for s in orders greater than 1
+      if(p[i] == "1") {
+        S <- paste(S, s1, "", smul, svar, "^", n-i+1, sep="")
+      } else {
+        S <- paste(S, s1, p[i], smul, svar, "^", n-i+1, sep="")
+      }
+    } else if (i == n) {
+      #added to clean out any coefficients for s in order 1
+      if(p[i] == "1") {
+        S <- paste(S, s1, "", smul, svar, sep="")
+      } else {
+        S <- paste(S, s1, p[i], smul, svar, sep="")
+      }
+    } else {
+      S <- paste(S, s1, p[i], sep="")
+    }
+  }
+  return(S)
+}
---
+++ R/tf.R
@@ -0,0 +1,74 @@
+# This function creates a tranfer function sys list structure with inputs,
+#numerator and denominator as vectors
+# Usage:
+# sys <- tf(c(0,1), c(1,2,3))
+# sys$num
+# sys$den
+#' @title Create Transfer function Model.
+#'
+#' @description
+#' \code{tf} creates the model for a transfer function
+#'
+#' @details
+#' \code{tf} creates a model object for transfer functions, Where \code{num} is the numerator and \code{den} is the denominator
+#' of the transfer function. If the transfer function is not proper, it returns a list with length(num) = length(den).
+#'
+#'
+#' @param num   A numeric vector
+#' @param den   A numeric vector
+#'
+#' @return Returns an object of 'tf' class list with a proper transfer function or with warnings when not proper.
+#'
+#' @examples
+#' tf(1, c(1,2,1))
+#' sys1 <- tf(1, c(1,2,1))
+#' sys1$num
+#' sys1$den
+#'
+#' @export
+
+tf <- function(num, den, Ts=NULL){
+
+  if((nargs() < 2) || (nargs() > 3)){
+    stop("tf: Incorrect number of inputs")
+  }
+    Dum <- tfchk(matrix(num,nrow = 1),matrix(den,nrow = 1));
+    num1 <- Dum$numc;
+    den1 <- Dum$denc;
+
+      if(is.null(Ts)){
+           cat("\nTransfer Function: Continuous time model", "\n\n")
+      } else {
+           cat("\nTransfer function: Discrete time model", "\n\n")
+      }
+
+    sys = list(num=num1, den=den1, Ts = Ts)
+    class(sys) <- "tf"
+    return(sys)
+}
+
+
+print.tf <- function(sys){
+  argnum <- c(sys$num)
+  argden <- c(sys$den)
+  numstr <- poly2str(argnum, svar = "s", smul = " ")
+  denstr <- poly2str(argden, svar = "s", smul = " ")
+  numlen <- nchar(numstr)
+  denlen <- nchar(denstr)
+  len <- max(numlen, denlen)
+  if(numlen < len){
+    center <- round((len - numlen)/2)
+    cat(rep(" ", center), numstr,"\n")
+  } else {
+    cat("   ", numstr,"\n")
+  }
+  cat(" " ,rep("-",round((len/2))+3))
+  cat(sprintf("\n"))
+  if(denlen < len){
+    center <- round((len - denlen)/2)
+    cat(rep(" ", center), denstr, "\n")
+  } else {
+    cat("   ", denstr, "\n")
+  }
+  cat(sprintf("\n\n"))
+}

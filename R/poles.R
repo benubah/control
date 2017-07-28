d418a560a13f4c4d741b0ac5a1ddaf96deb02678
benubah <ben@rpowerlabs.org>
2017-07-28 10:13:25 +0100

    added roxygen doc

--- R/poles.R
+++ R/poles.R
@@ -1,13 +1,20 @@
-# Obtain system poles for tf, ss and zpk
-# If sys is a transfer function, it computes the roots of the denominator
-# If sys is a state-space object, it computes the eigenvalues of the A matrix
-# If sys is a zpk object, it retrieves the poles from the object.
-
-#Example:
-# H1 <- tf(c(2, 5, 1),c(1, 3, 5))
-# pole(zpk(NULL, c(-1,-1), 1))
-# pole(ssdata(tf(1, c(1,2,1))))
-
+#' @title Obtain Poles for a System
+#'
+#'
+#' @description  This function obtains the poles for a given system
+#'
+#' @details  \code{pole} returns the poles for a given system either a transfer function,
+#' state-space or zero-pole models. If sys is a transfer function, it computes the roots of the denominator
+#' If sys is a state-space object, it computes the eigenvalues of the A matrix.
+#' If sys is a zpk object, it retrieves the poles from the object.
+#'
+#' @return The function returns a column matrix containing the poles for the given system
+#'
+#' @examples
+#' H1 <- tf(c(2, 5, 1),c(1, 3, 5))
+#' pole(zpk(NULL, c(-1,-1), 1))
+#' pole(ssdata(tf(1, c(1,2,1))))
+#'
 #' @export
 pole <- function(sys) {
   if (class(sys) == 'tf') {

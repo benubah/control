96e12fc8e6d501b7ab1cab4570cf2d5565c4eca8
benubah <ben@rpowerlabs.org>
2017-08-05 01:01:10 +0100

    eigenvalue sort

---
+++ R/esort.R
@@ -0,0 +1,21 @@
+# esort.R
+#
+# usage: a <- esort(p)
+#
+# This function sorts the complex continuous eigenvalues in descending
+# order. It sorts based on the real part. The unstable eigenvalues (positive
+# real part) will appear first.
+#
+# The function passes back the sorted eigenvalues and the cooresponding
+# indices in a list:
+#
+#    a$s   = sorted eigenvalues
+#    a$idx = index
+#
+#------------------------------------------------------------------------------
+
+esort <- function(p) {
+  t <- sort(-Re(p), index.return = TRUE)
+  return(list(s = p[t$ix], idx = t$ix))
+}
+

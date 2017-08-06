2edccb1ea50f953baa26622a0c850665b0432c29
benubah <ben@rpowerlabs.org>
2017-08-06 22:06:32 +0100

    added damp

---
+++ R/damp.R
@@ -0,0 +1,63 @@
+# damp.R
+#
+# Usage: damp(sys)
+#
+#
+# This function computes the natural frequency and damping for
+# continuous systems. It prints a table of the eigenvalues of
+# the matrix a, the associated damping factors, the associated natural
+# frequency (rad/s and Hz.).
+#
+# The function returns the natrural frequencies and damping
+# factors in a list:
+#
+#  omegan = Natural Frequencies (rad/s)
+#  zeta   = Damping Factors
+#
+
+damp <- function (sys, doPrint = TRUE) {
+  # Compute the eigenvalues of the matrix and sort them
+  if (class(sys) == 'ss') {
+    eigvlsort <- esort(eigen(sys[[1]])$values)$s
+  } else if (class(sys) == 'tf') {
+    eigvlsort <- esort(pracma::roots(c(sys[[1]])))$s
+  }   else if (class(sys) == 'zpk') {
+    eigvlsort <- esort(sys$p)$s
+  }    else{
+    stop("DAMP: sys must be of tf, ss or zpk class")
+  }
+
+
+  # Compute the natural frequency on rad/s.
+  omegan=abs(eigvlsort)
+
+  omegan = as.matrix(omegan)
+
+  # Compute the damping factor
+  zeta=-cos(atan2(Im(eigvlsort),Re(eigvlsort)))
+
+  # Compute the natural frequency (Hz).
+  omegahz <- sqrt(eigvlsort)/(2.0*pi)
+ if (doPrint) {
+  cat(sprintf("\n"))
+  cat(sprintf("     Eigenvalue            Damping     Freq. (rad/s)    Freq. (Hz). \n"))
+  cat(sprintf("     ----------            -------     -------------    -----------\n"))
+  for (i in 1:nrow(omegan)) {
+    if (Im(eigvlsort[i]) < 0.0) {
+      cat(sprintf("%-10.2f -%10.2fj", Re (eigvlsort[i]), abs(Im(eigvlsort[i]))))
+    }  else {
+      cat(sprintf("%-10.2f +%10.2fj", Re (eigvlsort[i]), abs(Im(eigvlsort[i]))))
+    }
+    cat(sprintf("   "))
+    cat(sprintf("%-10.2f", zeta[i]))
+    cat(sprintf("     "))
+    cat(sprintf("%-10.2f", omegan[i]))
+    cat(sprintf("      "))
+    cat(sprintf("%-10.2f", omegahz[i]))
+    cat(sprintf("\n"))
+  }
+  cat(sprintf("\n"))
+ }
+  return(list(omega = omegan, zeta = zeta))
+}
+

065940c79a91c9ba9af66cde52b06b7c489e4088
benubah <ben@rpowerlabs.org>
2017-08-24 00:39:19 +0100

    added doc

--- R/impulseplot.R
+++ R/impulseplot.R
@@ -1,16 +1,8 @@
-# Example:
-# A <- rbind(c(-2, -1), c(1,0)); B <- rbind(1,0);
-#          C <- cbind(0,1); D <- as.matrix(0);
-# impulseplot(ss(A,B,C,D))
-# MIMO
-# A1 <- rbind(c(0,1), c(-25,-4)); B1 <- rbind(c(1,1), c(0,1))
-# C1 <- rbind(c(1,0), c(0,1)); D1 <- rbind(c(0,0), c(0,0))
-# impulseplot(ss(A1,B1,C1,D1), 1:2)
+# Impulse Response Plotting utility
 
-#par(mfrow = c(2,2)) #run first to see all plots for 2-input, 2-output MIMO
 #' @export
-impulseplot <- function(sys, input = 1, t = NULL) {
-  res <- impulse(sys, input[1], t)
+impulseplot <- function(sys, t = NULL, input = 1) {
+  res <- impulse(sys, t, input[1])
   y <- res$y
   t <- res$t
 
@@ -25,13 +17,11 @@ impulseplot <- function(sys, input = 1, t = NULL) {
         plot(t, y[i, ], type = "l", col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Impulse response")
         grid(5, 5)
       }
-
     }
   }
   if (length(input) > 1) {
-
     for(i in 1:length(input)){
-      impulseplot(sys, i)
+      impulseplot(sys, t, i)
     }
   }
   par(mfrow = c(1,1))

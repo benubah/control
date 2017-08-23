82df86594eb3e154cc051d11fff39ca830446f9b
benubah <ben@rpowerlabs.org>
2017-08-23 20:37:51 +0100

    updated to use time vector for MIMO

--- R/stepplot.R
+++ R/stepplot.R
@@ -1,12 +1,18 @@
 # Step Response Plot
 #' @export
 stepplot <- function (sys, t = NULL, input = 1) {
-  if(is.null(t)) {
-    t <- seq(0, 5, 0.01)
-  }
-  res <- step(sys, t, input[1])
-  y <- res$y
 
+  if(is.null(t)) {
+    tf <- seq(0, 5, 0.01)
+  }
+  if(!is.null(t)) {
+    tf <- t
+
+  }
+
+  res <- step(sys, tf, input[1])
+  y <- res$y
+  t <- res$t
   if (length(input) == 1) {
   # Plot the results for all rows of y against t
      if (nrow(y) == 1) {
@@ -23,7 +29,7 @@ stepplot <- function (sys, t = NULL, input = 1) {
   }
   if (length(input) > 1) {
     for(i in 1:length(input)){
-      stepplot(sys, input = i)
+      stepplot(sys, tf, input = i)
       }
   }
 }

30f6218d52741add8ce7d2bf157964da47b5f201
benubah <ben@rpowerlabs.org>
2017-12-08 16:01:30 +0100

    added documentation

--- R/feedback.R
+++ R/feedback.R
@@ -1,48 +1,51 @@
-#----------------------------------------------------------------------
-#
-# feedback.R
-#
-# Usage: fdsys <- feedback(sys1, sys2, e, f)
-#
-# Feedback connection of two systems.
-#
-#         u-->O-->[System1]---+--->y
-#             |               |
-#             +---[System2]<--+
-#
-# fdsys <- feedback(sys1, sys2, SIGN) produces an
-# aggregate state-space system consisting of the feedback connection
-# of the two systems 1 and 2.  Typically, system 1 is a plant and
-# system 2 is a compensator.   If SIGN=1 then positive feedback is
-# used. If SIGN=-1 then negative feedback is used.  In all cases,
-# the resulting system has the same inputs and outputs as system 1.
-#
-# fdsys <- feedback(sys1, sys2, inputs, outputs)
-# produces the feedback system formed by feeding all the outputs of
-# system2 into the inputs of system 1 specified by INPUTS1 and by
-# feeding the outputs of system 2 specified by OUTPUTS1 into all the
-# inputs of system 2.  Positive feedback is assumed.  To connect
-# with negative feedback, use negative values in the vector INPUTS1.
-#
-# When sys1 and sys2 are transfer functions, feedback(sys1, sys2, SIGN) produces the SISO
-# closed loop system in transfer function form obtained by
-# connecting the two SISO transfer function systems in feedback
-# with the sign SIGN.
-#
-# See also: cloop, parallel, series, and connect.
-#
-
-# C <- pid(350,300,50)
-# P <- TF(" 1/(s^2 + 10* s + 20)")
-# feedback(C,P)
-# feedback(P,P,1)
-# feedback(P,P,-1)
-# feedback(P,P)
-# feedback(TF("C*P")) # On Octave: feedback(C*P)
-# feedback(sys2, tf(1,c(1,2,1)))
-
-# Similar to Octave, feedback(C, P)
-
+#' @title Feedback Connection of LTI systems
+#'
+#' @aliases fdbcksys
+#'
+#' @description
+#' \code{feedback} forms a feedback connection for two LTI state-space or transfer function systems
+#'
+#' @details
+#' When \code{sys1} and \code{sys2} are transfer functions \code{feedback(sys1, sys2, SIGN)} produces the SISO
+#' closed loop system in transfer function form obtained by
+#' connecting the two SISO transfer function systems in feedback
+#' with the sign SIGN.
+#'
+#' \code{feedback(sys1, sys2, SIGN)} produces an
+#' aggregate state-space system consisting of the feedback connection
+#' of the two systems 1 and 2.    If \code{SIGN = 1} then positive feedback is
+#' used. If \code{SIGN = -1} then negative feedback is used.  In all cases,
+#' the resulting system has the same inputs and outputs as system 1.
+#'
+#' \code{feedback(sys1, sys2, inputs, outputs)}
+#' produces the feedback system formed by feeding all the outputs of
+#' system2 into the inputs of system 1 specified by INPUTS1 and by
+#' feeding the outputs of system 2 specified by OUTPUTS1 into all the
+#' inputs of system 2.  Positive feedback is assumed.  To connect
+#' with negative feedback, use negative values in the vector INPUTS1.
+#'
+#' \code{feedback()} calls \code{fdbcksys()} to perform the feedback connection for two systems.
+#' Unity feedback calls are possile, for example, \code{feedback(sys1, 1)}, \code{feedback(1, sys1)}
+#'
+#' @param sys1   LTI system model of transfer-function or state-space model
+#' @param sys2   LTI system model of transfer-function or state-space model
+#' @param in1      vector of inputs
+#' @param out1      vector of outputs
+#'
+#' @return Returns the feedback system in \code{tf} or \code{ss} model
+#'
+#' @seealso \code{\link{cloop}} \code{\link{parallel}} \code{\link{series}}
+#'
+#' @examples
+#' C <- pid(350,300,50)
+#' P <- TF(" 1/(s^2 + 10* s + 20)")
+#' feedback(C,P)
+#' feedback(P,P,1)
+#' feedback(P,P,-1)
+#' feedback(P,P)
+#' feedback(P,1)
+#' feedback(TF("C*P")) # On Octave: feedback(C*P)
+#'
 #' @export
 feedback <- function(sys1, sys2, in1, out1){
 
@@ -108,11 +111,11 @@ fdbcksys <- function(sys1, sys2, in1, out1) {
     sys2 <- ssdata(sys2)
     errmsg <- abcdchk(sys1)
     if(errmsg != "") {
-      stop("Parallel: System 1: " + errmsg)
+      stop("Feedback: System 1: " + errmsg)
     }
     errmsg <- abcdchk(sys2)
     if(errmsg != "") {
-      stop("Parallel: System 2 " + errmsg)
+      stop("Feedback: System 2 " + errmsg)
     }
     num_y1 <- nrow(sys1$D)
     num_u1 <- ncol(sys1$D)
@@ -141,7 +144,7 @@ fdbcksys <- function(sys1, sys2, in1, out1) {
       outputs2 = (1:num_y2) + num_y1
     }
     if ((max(dim(as.matrix(outputs1))) != max(dim(as.matrix(inputs2)))) || (max(dim(as.matrix(outputs2)))!=max(dim(as.matrix(inputs1))))) {
-      stop("Parallel: Feedback connection sizes mismatch.")
+      stop("Feedback: Feedback connection sizes mismatch.")
     }
     # Create feedback system
     appsys <- append(sys1, sys2)

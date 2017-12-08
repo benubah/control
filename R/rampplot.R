00a7ec9209a7d4a0b897945404e5e057688e36cb
benubah <ben@rpowerlabs.org>
2017-12-08 16:07:27 +0100

    call graphics package

--- R/rampplot.R
+++ R/rampplot.R
@@ -9,15 +9,15 @@ rampplot <- function (sys, t = NULL, input = 1) {
  if (length(input) == 1) {
  # Plot the results for all rows of y against t
  if (nrow(y) == 1) {
-   plot(t, y, type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y"), main = "Ramp response")
-   grid(5, 5)
+   graphics::plot(t, y, type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y"), main = "Ramp response")
+   graphics::grid(5, 5)
  } else {
-   par(mfrow = c(1, nrow(y)))
+   graphics::par(mfrow = c(1, nrow(y)))
    for (i in 1:nrow(y)) {
-     plot(t, y[i, ], type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Ramp response")
-     grid(5, 5)
+     graphics::plot(t, y[i, ], type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Ramp response")
+     graphics::grid(5, 5)
    }
-   par(mfrow = c(1,1))
+   graphics::par(mfrow = c(1,1))
  }
 }
  if (length(input) > 1) {

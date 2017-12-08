01430a3ba48694161e9ed92e6a5f3f0d7aa125f8
benubah <ben@rpowerlabs.org>
2017-12-08 16:09:25 +0100

    call graphics package

--- R/stepplot.R
+++ R/stepplot.R
@@ -16,15 +16,15 @@ stepplot <- function (sys, t = NULL, input = 1) {
   if (length(input) == 1) {
   # Plot the results for all rows of y against t
      if (nrow(y) == 1) {
-       plot(t, y, type = "l", col = "blue", xlab = "Time, sec", ylab = paste("y"), main = "Step response")
-       grid(5, 5)
+       graphics::plot(t, y, type = "l", col = "blue", xlab = "Time, sec", ylab = paste("y"), main = "Step response")
+       graphics::grid(5, 5)
       } else {
-          par(mfrow = c(1, nrow(y)))
+        graphics::par(mfrow = c(1, nrow(y)))
         for (i in 1:nrow(y)) {
-          plot(t, y[i, ], type = "l", col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Step response")
-          grid(5, 5)
+          graphics::plot(t, y[i, ], type = "l", col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Step response")
+          graphics::grid(5, 5)
         }
-        par(mfrow = c(1,1))
+        graphics::par(mfrow = c(1,1))
       }
   }
   if (length(input) > 1) {

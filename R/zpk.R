c5e9502aac844fb5b614580e13f8c05a558a09a6
benubah <ben@rpowerlabs.org>
2017-12-08 16:41:29 +0100

    change sys to x

--- R/zpk.R
+++ R/zpk.R
@@ -52,34 +52,35 @@ zpk <- function(zero, pole, gain, Ts=NULL) {
 }
 
 #' @export
-print.zpk <- function(sys) {
-  if(class(sys) != 'zpk'){
+print.zpk <- function (x, ...) {
+  # arg x is the same sys
+  if(class(x) != 'zpk'){
     stop("print.zpk: sys must be a zpk object!")
   }
   #print(length(sys$k))
-  for (i in 1:length(sys$k)){
+  for (i in 1:length(x$k)){
     cat(paste("y",i,":", sep = ''))
-  if (is.null(sys$z[i]) || length(sys$z[i]) == 0) {
+  if (is.null(x$z[i]) || length(x$z[i]) == 0) {
     numstr <- " "
   } else {
 
-    if (!is.complex(sys$z[i])) {
-      ztmp <- ifelse (sys$z[i] != 0, sys$z[i], NA)
-      z2 <- na.omit(ztmp)
+    if (!is.complex(x$z[i])) {
+      ztmp <- ifelse (x$z[i] != 0, x$z[i], NA)
+      z2 <- stats::na.omit(ztmp)
       numstr <- paste("(", sprintf("s%+g", z2), ")", sep="")
-    } else if (is.complex(sys$z[i])) {
-      numstr <- paste("(s", sprintf("%+g%+g%s",Re(sys$z[i]), Im(sys$z[i]), "j"), ")", sep="")
+    } else if (is.complex(x$z[i])) {
+      numstr <- paste("(s", sprintf("%+g%+g%s",Re(x$z[i]), Im(x$z[i]), "j"), ")", sep="")
     }
 
   }
 
-  if (!is.complex(sys$p)) {
-    denstr <- paste("(s", sprintf("%+g",sys$p), ")", sep="")
-  } else if (is.complex(sys$p)) {
-    denstr <- paste("(s", sprintf("%+2g%+2g%s", Re(sys$p), Im(sys$p), "j"),  ")", sep="")
+  if (!is.complex(x$p)) {
+    denstr <- paste("(s", sprintf("%+g",x$p), ")", sep="")
+  } else if (is.complex(x$p)) {
+    denstr <- paste("(s", sprintf("%+2g%+2g%s", Re(x$p), Im(x$p), "j"),  ")", sep="")
   }
 
-  numstr <- paste(c(sys$k[i],numstr), collapse = " ")
+  numstr <- paste(c(x$k[i],numstr), collapse = " ")
   numlen <- nchar(numstr)
   denlen <- nchar(denstr)
   numlen <- sum(numlen)
@@ -104,10 +105,10 @@ print.zpk <- function(sys) {
 
   }
 
-  if ( is.null(sys$Ts) || sys$Ts <= 0) {
+  if ( is.null(x$Ts) || x$Ts <= 0) {
     cat(" Zero-Pole-Gain: Continuous time model", "\n")
   } else {
-    cat("Sample Time =", sys$Ts, "\n")
+    cat("Sample Time =", x$Ts, "\n")
     cat("Zero-Pole-Gain: Discrete time model", "\n\n")
   }
 }

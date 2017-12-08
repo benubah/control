296c85f1f195f98b5a3f92eb78fc8b919db7c23f
benubah <ben@rpowerlabs.org>
2017-12-08 16:02:48 +0100

    use graphics package call

--- R/initialplot.R
+++ R/initialplot.R
@@ -7,14 +7,14 @@ initialplot <- function (sys, x0 = NULL, t = NULL) {
   t <- res$t
   # Plot the results for all rows of y against t
   if (nrow(y) == 1) {
-    plot(t, y, type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y"), main = "Initial response")
-    grid(5, 5)
+    graphics::plot(t, y, type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y"), main = "Initial response")
+    graphics::grid(5, 5)
   } else {
-    par(mfrow = c(nrow(y), 1))
+    graphics::par(mfrow = c(nrow(y), 1))
     for (i in 1:nrow(y)) {
-      plot(t, y[i, ], type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Initial response")
-      grid(5, 5)
+      graphics::plot(t, y[i, ], type = "l", lwd = 2, col = "blue", xlab = "Time, sec", ylab = paste("y", i), main = "Initial response")
+      graphics::grid(5, 5)
     }
-    par(mfrow = c(1,1))
+    graphics::par(mfrow = c(1,1))
   }
 }

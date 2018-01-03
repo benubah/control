3328a460c08ef68e0cb606d9e519d4003b9de4a9
benubah <ben@rpowerlabs.org>
2018-01-03 02:15:21 +0100

    exported lyap and dlyap

--- R/bodeplot.R
+++ R/bodeplot.R
@@ -2,11 +2,12 @@
 
 #' @export
 bodeplot <- function(sys,  w = seq(0, 100, length=10000), iu = 1, subtitle = "In(1) Out(1)") {
- if (issiso(sys)) {
+ if (class(sys) != 'ss' || issiso(sys)) {
   resp <- bode(sys, w, iu)
   w <- resp$w
   mag <- resp$mag
   phase <- resp$phase
+  graphics::par(mfrow = c(2,1))
   #magnitude plot
   pracma::semilogx(w, mag, main = "Bode Magnitude", sub = subtitle, col="blue", xlab="Frequency [rad/sec]", ylab="Magnitude [dB]", type = 'l')
 
@@ -14,7 +15,7 @@ bodeplot <- function(sys,  w = seq(0, 100, length=10000), iu = 1, subtitle = "In
   pracma::semilogx(w, phase, main = "Bode Phase", sub = subtitle, col="blue", xlab="Frequency [rad/sec]", ylab="Phase [deg]", type = 'l')
  }
 
-  if(ismimo(sys)) {
+  if(class(sys) == 'ss' && ismimo(sys)) {
     graphics::par(mfrow = c( 2*nrow(sys[[4]]), ncol(sys[[4]]) ) )
     for (i in 1:nrow(sys[[4]])) {
       for (j in 1:ncol(sys[[4]])) {
